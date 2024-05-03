8784621858159afae899ffe8913d1a92df2cd89f
CUB3D <callumthom11@gmail.com>
2024-05-03 02:06:05 +0100

    MODELS FUCKING WORK

--- tpwstuff/src/main.rs
+++ tpwstuff/src/main.rs
@@ -1,191 +1,9 @@
 pub mod parse;
+pub mod wad;
+mod md2;
 
-use std::collections::HashMap;
-use std::ffi::CString;
-use crate::parse::{be_u16, le_f32, le_u16, le_u32, take};
-
-struct WadEntry {
-    compression: u8,
-    data: Vec<u8>,
-}
-
-struct WadFile {
-    wad: HashMap<String, WadEntry>
-}
-
-impl WadFile {
-    pub fn new(p: &str) -> Self {
-        let mut wad = HashMap::new();
-
-        let w = std::fs::read(p).unwrap();
-        let (i, _) = take(&w, 4);
-        let (i, _v) = le_u32(i);
-        let (i, _p) = take(i, 64);
-        let (i, fc) = le_u32(i);
-        let (i, off) = le_u32(i);
-        let (i, fll) = le_u32(i);
-        let (i, _p1) = le_u32(i);
-
-
-        let mut i = i;
-        for _ in 0..fc {
-            let (j, _) = le_u32(i);
-            let (j, fno) = le_u32(j);
-            let (j, fnl) = le_u32(j);
-
-            let filename_byte = &w[fno as usize..][..fnl as usize];
-            let filename = CString::from_vec_with_nul(filename_byte.to_vec()).unwrap().to_string_lossy().to_string();
-
-            let (j, dof) = le_u32(j);
-            let (j, fl) = le_u32(j);
-            let (j, c ) = le_u32(j);
-            let (j, _ds) = le_u32(j);
-            let (j, _p) = take(j, 12);
-
-            // if !filename.contains("MD2") {
-            println!("fn = {filename:?}, c={c}");
-
-            let data = w[dof as usize..][..fl as usize].to_vec();
-
-            wad.insert(filename, WadEntry {
-                compression: c as u8,
-                data,
-            });
-            // }
-
-
-            i = j;
-        }
-
-        println!("version: {_v}, file cnt: {fc}, off={off}, fll={fll}");
-
-        Self {
-            wad
-        }
-    }
-
-    pub fn get_data(&self, name: &str) -> Vec<u8> {
-        let w = self.wad.get(name).unwrap();
-
-        if w.compression == 0 {
-            w.data.clone()
-        } else {
-            {
-                fn DecompressData( data: &[u8], outputData: &mut Vec<u8>, offset: usize, opcodeLength: usize, proceedingDataLength: usize, referencedDataLength: usize, referencedDataOffset: usize )
-                {
-                    for i in 0..proceedingDataLength  // Proceeding data comes from the source buffer (compressed data)
-                    {
-                        let pos = (offset + opcodeLength + i);
-                        if ( pos >= data.len() ) {
-                            break; // Prevent any overflowing
-                        }
-
-                        outputData.push( data[pos] );
-                    }
-
-                    let outputDataLen = outputData.len();
-
-                    for i in 0..referencedDataLength // Referenced data comes from the output buffer (decompressed data)
-                    {
-                        if (referencedDataOffset > outputDataLen) {
-                            break;
-                        }
-                        let pos = (outputDataLen - referencedDataOffset);
-
-                        outputData.push( outputData[pos + i] );
-                    }
-                }
-
-
-                let mut out = Vec::new();
-
-                let (i, h) = be_u16(&w.data);
-                if(h != 0x10FB) {
-                    panic!();
-                }
-                let (_i, _) = take(i, 3);
-
-                let mut pos = 5;
-                loop {
-                    let cmd = w.data[pos];
-                    // println!("{cmd:b}");
-
-                    pos += 1;
-
-                    let offset = pos - 1;
-
-                    if(cmd & 0b1000_0000 == 0b0000_0000) {
-                        let data = &w.data;
-
-                        let proceedingDataLength = ((data[offset] & 0x03));
-                        let referencedDataLength = (((data[offset] & 0x1C) >> 2) + 3);
-                        let referencedDataDistance = ((((data[offset] & 0x60) as usize) << 3) + data[offset + 1] as usize + 1);
-                        let skipAhead = proceedingDataLength;
-
-                        // println!("pdl = {proceedingDataLength}, rdl = {referencedDataLength}, rdd={referencedDataDistance}");
-
-                        DecompressData(data, &mut out, offset, 2, proceedingDataLength as usize, referencedDataLength as usize, referencedDataDistance as usize);
-                        pos += (2 + skipAhead - 1) as usize;
-                        // println!("2bc");
-
-
-                    } else {
-                        if ((cmd & 0x1F) + 1) << 2 <= 0x70 && cmd & 0b1110_0000 == 0b1110_0000 {
-                            let proceedingDataLength = (((cmd & 0x1F) + 1) << 2);
-                            DecompressData(&w.data, &mut out, pos - 1, 1, proceedingDataLength as usize, 0, 0);
-                            pos += (proceedingDataLength - 1 + 1) as usize;
-                            // println!("obc");
-                        } else {
-                            if cmd & 0b1110_0000 == 0b1100_0000 {
-                                let data = &w.data;
-                                let proceedingDataLength = ((data[offset] & 0x03));
-                                let referencedDataLength = (((data[offset] as usize & 0x0C) << 6) + data[offset + 3] as usize + 5);
-                                let referencedDataDistance = (((data[offset] as usize & 0x10) << 12) + ((data[offset + 1] as usize) << 8) + data[offset + 2] as usize + 1);
-                                let skipAhead = proceedingDataLength;
-
-
-
-                                DecompressData(data, &mut out, offset, 4, proceedingDataLength as usize, referencedDataLength as usize, referencedDataDistance as usize);
-                                pos += (4 - 1 + skipAhead) as usize;
-
-                                // println!("4bc");
-                            } else {
-                                if cmd & 0b1100_0000 == 0b1000_0000 {
-                                    let data = &w.data;
-                                    let proceedingDataLength = ((data[offset + 1] & 0xC0) >> 6);
-                                    let referencedDataLength = ((data[offset] & 0x3F) + 4);
-                                    let referencedDataDistance = ((((data[offset + 1] & 0x3F) as usize) << 8) + data[offset + 2] as usize + 1);
-                                    let skipAhead = proceedingDataLength;
-
-
-
-                                    DecompressData(data, &mut out, offset, 3, proceedingDataLength as usize, referencedDataLength as usize, referencedDataDistance as usize);
-                                    pos += (3 - 1 + skipAhead) as usize;
-
-                                    // println!("3bc");
-                                } else {
-                                    if ((cmd & 0x1F) + 1) << 2 > 0x70 && cmd & 0b1110_0000 == 0b1110_0000 {
-                                        let data = &w.data;
-
-                                        let proceedingDataLength = ((data[offset] & 0x03));
-                                        // let skipAhead = proceedingDataLength;
-                                        DecompressData(data, &mut out, offset, 3, proceedingDataLength as usize, 0 as usize, 0 as usize);
-                                        // pos += (1 - 1 + skipAhead) as usize;
-                                        break;
-                                    } else {
-                                        panic!("bad cmd: {cmd:b}");
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                out
-            }
-        }
-    }
-}
+use crate::parse::{le_f32, le_u16, le_u32, take};
+use crate::wad::WadFile;
 
 fn main() {
     // let w = WadFile::new("/Users/cub3d/Downloads/mp/Theme Park World/data/levels/jungle/terrain.wad");
@@ -196,16 +14,18 @@ fn main() {
     // let d = w.get_data("gte_wal2.wct");
     // std::fs::write("./not_tpw/gte_wal2.wct", &d).unwrap();
 
-    // let w = WadFile::new("/Users/cub3d/Downloads/mp/Theme Park World/data/ui.wad");
+    // let w = WadFile::new("./ui.wad");
     // let d = w.get_data("tpwlogo.MD2");
     // std::fs::write("./not_tpw/tpwlogo.MD2", &d).unwrap();
+    // let d = std::fs::read("../tpwlogo.MD2").unwrap();
+    // let d = std::fs::read("../b_map.MD2").unwrap();
     // let d = w.get_data("b_map.MD2");
     // std::fs::write("./not_tpw/b_map.MD2", &d).unwrap();
 
-    let w = WadFile::new("./volcano.wad");
-    let d = w.get_data("volcano.MD2");
+    // let w = WadFile::new("./volcano.wad");
+    // let d = w.get_data("volcano.MD2");
 
-    // let w = WadFile::new("/Users/cub3d/Downloads/mp/Theme Park World/data/levels/jungle/rides/monkey.wad");
+    // let w = WadFile::new("monkey.wad");
     // let d = w.get_data("monkey.MD2");
 
     // let d = w.get_data("volcanoc.MD2");
@@ -217,6 +37,9 @@ fn main() {
     //
     // let w = WadFile::new("./incagod.wad");
     // let d = w.get_data("incagod.MD2");
+
+    let wad = WadFile::new("/mnt/Data/Programs/themeparkworld/data/levels/jungle/rides/tvsim.wad");
+    let d = wad.get_data("Ptvsim.md2");
 
 
     /*
@@ -442,7 +265,7 @@ u16 vals[3];
         let (i, _off9) = le_u32(i);
         let (i, mesh_ptr) = le_u32(i);
         let (i, _off11) = le_u32(i);
-        let (i, _off12) = le_u32(i);
+        let (_i, _off12) = le_u32(i);
 
 
         // Mesh
@@ -451,24 +274,28 @@ u16 vals[3];
         println!("mesh_dat = {:x}, mesh_cnt={mesh_cnt}", mesh_ptr + 21*4);
 
 
-        #[derive(Default, Clone)]
-        struct Mesh {
+        #[derive(Default, Clone, Debug)]
+        #[allow(dead_code)]
+        pub struct Mesh {
             name: String,
             posoff: u32,
             vert_cnt: u32,
             xoff: u32,
             yoff: u32,
-            y_cnt: u32,
+            face_cnt: u32,
+            idk_1: u32,
+            vertex_order_len: u32,
+            vertex_order_offset: u32,
         }
         let mut meshes = vec![Mesh::default(); mesh_cnt as usize];
         let mut i = mesh_data;
         for mesh_idx in 0..mesh_cnt {
             let (j, _) = take(i, 21 * 4); // +0
             let (j, _noff) = le_u32(j); //+54
-            let (j, _) = le_u16(j);
+            let (j, idk_1) = le_u16(j);
             let (j, vert_cnt) = le_u16(j);
             let (j, y_cnt) = le_u16(j);
-            let (j, _) = le_u16(j);       // some_count
+            let (j, z_cnt) = le_u16(j);       // some_count
             let (j, _posoff) = le_u32(j); //5c
             let (j, _idk) = le_u32(j); // 60
             let (j, _uvoff) = le_u32(j); //64
@@ -482,13 +309,13 @@ u16 vals[3];
             let (j, _34) = le_f32(j);
             let (j, _35) = le_f32(j);
             let (j, _36) = le_f32(j);
-            let (j, _37) = le_f32(j);
+            let (j, zoff) = le_u32(j);
             let (j, _38) = le_f32(j);
             let (j, _39) = le_f32(j);
 
             println!("Mesh {mesh_idx}");
 
-            let mut name = d[_noff as usize..][..8].to_vec();
+            let name = d[_noff as usize..][..8].to_vec();
             let mut name_s = String::new();
             for b in name {
                 if b == 0 {
@@ -508,25 +335,44 @@ u16 vals[3];
                 posoff: _posoff,
                 xoff: _xoff,
                 yoff: _yoff,
-                y_cnt: y_cnt as _,
+                face_cnt: y_cnt as _,
+                idk_1: idk_1 as _,
+                vertex_order_len: z_cnt as _,
+                vertex_order_offset: zoff as _,
             };
 
             i = j;
         }
 
-        let msh_idx = 0;
+        let msh_idx = 2;
         let msh = meshes.get(msh_idx).unwrap();
+        println!("Exporting: {msh:?}");
         let msh_pos_end = meshes.get(msh_idx+1).map(|m| m.posoff).unwrap_or(meshes.get(0).unwrap().xoff);
         let cnt = (msh_pos_end - msh.posoff) / (3*4*4);
         println!("msh pnt cnt = {cnt}");
+        println!("idk1 = {}", msh.idk_1);
 
         let pos_data = &d[msh.posoff as usize..][..];
 
         let mut i = pos_data;
 
-        let mut tris = Vec::new();
-        for _ in 0..cnt {
-            let elem = 4;
+        let mut verticies = Vec::new();
+        let mut c = msh.idk_1;
+        if c % 4 == 0 {
+            c = c;
+        } else if c % 2 == 0 {
+            c = c + 2;
+        } else {
+            c = c + 3;
+        }
+
+
+        loop {
+            let elem = if c > 4 {
+                4
+            } else {
+                c
+            } as usize;
             let mut points = vec![(0_f32, 0_f32, 0_f32); elem];
             for idx in 0..elem {
                 let (j, f) = le_f32(i);
@@ -544,151 +390,67 @@ u16 vals[3];
                 i = j;
             }
 
-            tris.push(points);
+            verticies.extend_from_slice(&points);
+
+            if c < 4 {
+                break;
+            }
+
+            c -= 4;
+            if c == 0 {
+                break;
+            }
         }
 
+
+        println!("z_cnt = {}", msh.vertex_order_len);
+        println!("zoff = {}", msh.vertex_order_offset);
+
+        // Verticies in vertex section are not stored in order, they need to be re-mapped
+        // vertex at idx `x` in file data needs to be at position `vertex_order[x]`
+        // Faces are specified in terms of this re-mapped ordering
+        let mut vertex_order = vec![0u16; msh.vertex_order_len as _];
+        let vertex_order_data = &d[msh.vertex_order_offset as usize..][..msh.vertex_order_len as usize*2];
+        let mut i = vertex_order_data;
+        for idx in 0..msh.vertex_order_len as usize {
+            let (j, zv) = le_u16(i);
+            vertex_order[idx] = zv;
+            i = j;
+        }
+
+        // Re-order verticies to line up with expected order
+        let p = verticies.clone();
+        let mut verticies = Vec::new();
+        for x in vertex_order {
+            verticies.push(p[x as usize]);
+        }
+
+
+
+        println!("pnt cnt = {}", verticies.len() * 3);
+
+        // Parse face data
         let msh = meshes.get_mut(msh_idx).unwrap();
 
-        let face_data = &d[msh.yoff as usize..][..msh.y_cnt as usize *0x10];
+        let face_data = &d[msh.yoff as usize..][..msh.face_cnt as usize *8];
 
         let mut faces = Vec::new();
 
         let mut i = face_data;
-        // for x in 0..12 {
-        //     let (j, x) = le_u16(i);
-        //     faces.push(x);
-        //     i = j;
-        // }
-        for idx in 0..msh.y_cnt as usize {
+
+        for _idx in 0..msh.face_cnt as usize {
             let (j, _ptr) = le_u16(i);
+            let (j, a) = le_u16(j);
             let (j, b) = le_u16(j);
             let (j, c) = le_u16(j);
-            let (j, d) = le_u16(j);
-            // let (j, e) = le_u16(j);
-            // let (j, f) = le_u16(j);
-            // let (j, g) = le_u16(j);
 
-
-            // faces.push((b+1, c+1, d+1));
-            faces.push((b+1, c+1, d+1));
-            // if idx > 5 {
-            //     break;
-            // }
-            // faces.push((e, f, g));
-
+            faces.push((a+1, b+1, c+1));
 
             i = j;
         }
-        faces.clear();
 
-        // let face2 = faces;
-        // let mut faces = Vec::new();
-        // faces.push((face2[1]+1, face2[2]+1, face2[3]+1));
-
-
-        //f 6 1 5
-        // f 6 5 10
-        // 5 0 4
-        // 5 4 9
-
-        /*let mut i = face_data;
-        for idx in 0..msh.y_cnt as usize {
-
-            let (j, _ptr) = le_u16(i);
-            let (j, b) = le_u16(j);
-            let (j, c) = le_u16(j);
-            let (j, d) = le_u16(j);
-            // let (j, e) = le_u16(j);
-            // let (j, f) = le_u16(j);
-            // let (j, g) = le_u16(j);
-
-            faces[idx].0 = b;
-            faces[idx].1 = c;
-            faces[idx].2 = d;
-
-
-            i = j;
-        }*/
-
-        /*
-
-        let _yoff = 0;
-        let _xoff = 0;
-        let _posoff = 0;
-        let y_cnt = 0;
-
-        println!("pos = {_posoff}");
-
-        // mesh data
-        let cnt = ((_yoff - _xoff)/4) as usize;
-        println!("cnt = {cnt}");
-        let pos_data = &d[_posoff as usize..][..];
-
-        let mut i = pos_data;
-
-        println!("xoff = {_xoff}, yoff={_yoff} posoff={_posoff}");
-        let _posoff = _posoff + 24;
-        let cnt2 = (_xoff - _posoff);
-        println!("cnt2 = {cnt2}");
-
-        let elems = 4_usize;
-
-        let cnt_idk = (cnt2 as usize / (4*elems*3));
-
-        println!("idk = {}", (cnt2 as f32 / (4*elems*3) as f32));
-        println!("idk = {}", cnt_idk);
-
-        for _ in 0..cnt_idk {
-            let mut points = vec![(0_f32, 0_f32, 0_f32); elems];
-            for idx in 0..elems {
-                let (j, f) = le_f32(i);
-                points[idx].0 = f;
-                i = j;
-            }
-            for idx in 0..elems {
-                let (j, f) = le_f32(i);
-                points[idx].1 = f;
-                i = j;
-            }
-            for idx in 0..elems {
-                let (j, f) = le_f32(i);
-                points[idx].2 = f;
-                i = j;
-            }
-
-            tris.push(points);
-        }*/
-        /*
-        let face_data = &d[_yoff as usize..][..y_cnt as usize *0x10];
-
-
-        let mut faces = vec![(0u16, 0u16, 0u16); y_cnt as usize];
-
-        println!("fce cnt = {}", y_cnt);
-        let mut i = face_data;
-        for idx in 0..y_cnt as usize {
-
-            let (j, ptr) = le_u32(i);
-            let (j, b) = le_u16(j);
-            let (j, c) = le_u16(j);
-            let (j, d) = le_u16(j);
-            let (j, e) = le_u16(j);
-            let (j, f) = le_u16(j);
-            let (j, g) = le_u16(j);
-
-            faces[idx].0 = b;
-            faces[idx].1 = c;
-            faces[idx].2 = d;
-
-
-            i = j;
-        }*/
-
-
-        for tri in &tris {
-            for (x, y, z) in tri {
-                ooo.push_str(&format!("v {x} {y} {z}\n"));
-            }
+        for (x, y, z) in &verticies {
+            ooo.push_str(&format!("v {x} {y} {z}\n"));
         }
 
         for (x,y,z) in faces {
@@ -876,8 +638,9 @@ u16 y[cnt3*4] @  meshes[0].yoff;
     // let grd_top1 = img_hack(gl, "./not_tpw/grd_top1.wct.raw")?;
     // let grd_top1 = img_hack(gl, "./not_tpw/gte_wal1.wct.raw")?;
 
+    /*
 
-    let d = w.get_data("base.map");
+    // let d = w.get_data("base.map");
     // std::fs::write("./not_tpw/out.txt", &d);
     let (i, _) = take(&d, 0x24);
     let (i, mag) = take(i, 4);
@@ -964,7 +727,7 @@ u16 y[cnt3*4] @  meshes[0].yoff;
 
         // Save the image as “fractal.png”, the format is deduced from the path
         imgbuf.save("./not_tpw/map.png").unwrap();
-    }
+    }*/
 
  */
 }
---
+++ tpwstuff/src/wad.rs
@@ -0,0 +1,186 @@
+use std::collections::HashMap;
+use std::ffi::CString;
+use crate::parse::{be_u16, le_u32, take};
+
+struct WadEntry {
+    compression: u8,
+    data: Vec<u8>,
+}
+
+pub struct WadFile {
+    wad: HashMap<String, WadEntry>
+}
+
+impl WadFile {
+    pub fn new(p: &str) -> Self {
+        let mut wad = HashMap::new();
+
+        let w = std::fs::read(p).unwrap();
+        let (i, _) = take(&w, 4);
+        let (i, _v) = le_u32(i);
+        let (i, _p) = take(i, 64);
+        let (i, fc) = le_u32(i);
+        let (i, off) = le_u32(i);
+        let (i, fll) = le_u32(i);
+        let (i, _p1) = le_u32(i);
+
+
+        let mut i = i;
+        for _ in 0..fc {
+            let (j, _) = le_u32(i);
+            let (j, fno) = le_u32(j);
+            let (j, fnl) = le_u32(j);
+
+            let filename_byte = &w[fno as usize..][..fnl as usize];
+            let filename = CString::from_vec_with_nul(filename_byte.to_vec()).unwrap().to_string_lossy().to_string();
+
+            let (j, dof) = le_u32(j);
+            let (j, fl) = le_u32(j);
+            let (j, c ) = le_u32(j);
+            let (j, _ds) = le_u32(j);
+            let (j, _p) = take(j, 12);
+
+            // if !filename.contains("MD2") {
+            println!("fn = {filename:?}, c={c}");
+
+            let data = w[dof as usize..][..fl as usize].to_vec();
+
+            wad.insert(filename, WadEntry {
+                compression: c as u8,
+                data,
+            });
+            // }
+
+
+            i = j;
+        }
+
+        println!("version: {_v}, file cnt: {fc}, off={off}, fll={fll}");
+
+        Self {
+            wad
+        }
+    }
+
+    pub fn get_data(&self, name: &str) -> Vec<u8> {
+        let w = self.wad.get(name).unwrap();
+
+        if w.compression == 0 {
+            w.data.clone()
+        } else {
+            {
+                fn decompress_data(data: &[u8], output_data: &mut Vec<u8>, offset: usize, opcode_length: usize, proceeding_data_length: usize, referenced_data_length: usize, referenced_data_offset: usize )
+                {
+                    for i in 0..proceeding_data_length  // Proceeding data comes from the source buffer (compressed data)
+                    {
+                        let pos = offset + opcode_length + i;
+                        if pos >= data.len() {
+                            break; // Prevent any overflowing
+                        }
+
+                        output_data.push( data[pos] );
+                    }
+
+                    let output_data_len = output_data.len();
+
+                    for i in 0..referenced_data_length // Referenced data comes from the output buffer (decompressed data)
+                    {
+                        if referenced_data_offset > output_data_len {
+                            break;
+                        }
+                        let pos = output_data_len - referenced_data_offset;
+
+                        output_data.push( output_data[pos + i] );
+                    }
+                }
+
+
+                let mut out = Vec::new();
+
+                let (i, h) = be_u16(&w.data);
+                if h != 0x10FB {
+                    panic!();
+                }
+                let (_i, _) = take(i, 3);
+
+                let mut pos = 5;
+                loop {
+                    let cmd = w.data[pos];
+                    // println!("{cmd:b}");
+
+                    pos += 1;
+
+                    let offset = pos - 1;
+
+                    if cmd & 0b1000_0000 == 0b0000_0000 {
+                        let data = &w.data;
+
+                        let proceeding_data_length = data[offset] & 0x03;
+                        let referenced_data_length = ((data[offset] & 0x1C) >> 2) + 3;
+                        let referenced_data_distance = (((data[offset] & 0x60) as usize) << 3) + data[offset + 1] as usize + 1;
+                        let skip_ahead = proceeding_data_length;
+
+                        // println!("pdl = {proceeding_data_length}, rdl = {referenced_data_length}, rdd={referenced_data_distance}");
+
+                        decompress_data(data, &mut out, offset, 2, proceeding_data_length as usize, referenced_data_length as usize, referenced_data_distance as usize);
+                        pos += (2 + skip_ahead - 1) as usize;
+                        // println!("2bc");
+
+
+                    } else {
+                        if ((cmd & 0x1F) + 1) << 2 <= 0x70 && cmd & 0b1110_0000 == 0b1110_0000 {
+                            let proceeding_data_length = ((cmd & 0x1F) + 1) << 2;
+                            decompress_data(&w.data, &mut out, pos - 1, 1, proceeding_data_length as usize, 0, 0);
+                            pos += (proceeding_data_length - 1 + 1) as usize;
+                            // println!("obc");
+                        } else {
+                            if cmd & 0b1110_0000 == 0b1100_0000 {
+                                let data = &w.data;
+                                let proceeding_data_length = data[offset] & 0x03;
+                                let referenced_data_length = ((data[offset] as usize & 0x0C) << 6) + data[offset + 3] as usize + 5;
+                                let referenced_data_distance = ((data[offset] as usize & 0x10) << 12) + ((data[offset + 1] as usize) << 8) + data[offset + 2] as usize + 1;
+                                let skip_ahead = proceeding_data_length;
+
+
+
+                                decompress_data(data, &mut out, offset, 4, proceeding_data_length as usize, referenced_data_length as usize, referenced_data_distance as usize);
+                                pos += (4 - 1 + skip_ahead) as usize;
+
+                                // println!("4bc");
+                            } else {
+                                if cmd & 0b1100_0000 == 0b1000_0000 {
+                                    let data = &w.data;
+                                    let proceeding_data_length = (data[offset + 1] & 0xC0) >> 6;
+                                    let referenced_data_length = (data[offset] & 0x3F) + 4;
+                                    let referenced_data_distance = (((data[offset + 1] & 0x3F) as usize) << 8) + data[offset + 2] as usize + 1;
+                                    let skip_ahead = proceeding_data_length;
+
+
+
+                                    decompress_data(data, &mut out, offset, 3, proceeding_data_length as usize, referenced_data_length as usize, referenced_data_distance as usize);
+                                    pos += (3 - 1 + skip_ahead) as usize;
+
+                                    // println!("3bc");
+                                } else {
+                                    if ((cmd & 0x1F) + 1) << 2 > 0x70 && cmd & 0b1110_0000 == 0b1110_0000 {
+                                        let data = &w.data;
+
+                                        let proceeding_data_length = data[offset] & 0x03;
+                                        // let skipAhead = proceeding_data_length;
+                                        decompress_data(data, &mut out, offset, 3, proceeding_data_length as usize, 0 as usize, 0 as usize);
+                                        // pos += (1 - 1 + skipAhead) as usize;
+                                        break;
+                                    } else {
+                                        panic!("bad cmd: {cmd:b}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                out
+            }
+        }
+    }
+}
